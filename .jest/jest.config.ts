381b46592438fe80d374512ba51a563fd0578c2e
wtae1216 <wtae1216@gmail.com>
2020-11-19 03:41:24 +0900

    test 환경 세팅
     - jest: 기본 javascript test 라이브러리
     - jest-html-reporters: gui reporting 을 위한 라이브러리
     - ts-jest: jest 내 typescript 세팅을 위한 라이브러리
     - @testing-library/*: React Component test 를 위한 라이브러리

---
+++ .jest/jest.config.ts
@@ -0,0 +1,38 @@
+import { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    rootDir: '../',
+    modulePaths: ['<rootDir>/src/'],
+    setupFilesAfterEnv: ['<rootDir>/.jest/jest.setup.ts'],
+    globals: {
+        'ts-jest': {
+            tsconfig: '<rootDir>/.jest/tsconfig.json'
+        }
+    },
+    transform: {
+        '^.+\\.(ts|tsx)?$': 'ts-jest',
+        '^.+\\.[t|j]sx?$': 'babel-jest'
+    },
+    collectCoverage: true,
+    coverageReporters: ['json', 'html'],
+    collectCoverageFrom: [
+        '<rootDir>/src/components/**/*.{ts,tsx,js,jsx}',
+        '!<rootDir>/src/**/*.stories.{ts,tsx,js,jsx}'
+    ],
+    testMatch: ['**/(*.)+(test).[jt]s?(x)'],
+    reporters: [
+        'default',
+        [
+            'jest-html-reporters',
+            {
+                publicPath: 'report',
+                filename: 'report.html',
+                expand: false
+            }
+        ]
+    ]
+};
+
+export default config;
---
+++ .jest/jest.setup.ts
@@ -0,0 +1,4 @@
+import '@testing-library/jest-dom/extend-expect';
+
+// jest 타입아웃 30초로 설정
+jest.setTimeout(30000);
