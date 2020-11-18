331c0de2484db7484dbfdc324fcfe8b49df1f755
아이작_조서환 <wtae1216@afreecatv.com>
2020-11-18 19:05:54 +0900

    eslint, prettier 세팅

---
+++ src/components/Test.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const Test: FC = () => {
+    return <div>This is Test Component</div>;
+};
+
+export default Test;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import Test from 'components/Test';
+
 const root = document.getElementById('root');
 
 const App = () => {
     return (
-        <h1>This is Typing Game. SeoHwan Cho</h1>
+        <>
+            <h1>This is Typing Game. SeoHwan Cho</h1>
+            <Test />
+        </>
     );
 };
 
