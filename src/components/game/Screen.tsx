5de825f7fbe8727c7b4544c1f562adb75f2c4dea
wtae1216 <wtae1216@gmail.com>
2020-11-23 05:07:06 +0900

    Screen 컴포넌트 내 Ready 추가

--- src/components/game/Screen.tsx
+++ src/components/game/Screen.tsx
@@ -1,7 +1,38 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+import { useHistory } from 'libs/router';
+
+import { RootReducerState } from 'modules';
+
+import Status from 'constants/Status';
+
+import Ready from 'components/game/screen /Ready';
 
 const Screen: FC = () => {
-    return <div />;
+    const history = useHistory();
+
+    const gameStatus = useSelector<
+        RootReducerState,
+        RootReducerState['gameReducer']['status']
+    >(({ gameReducer }) => gameReducer.status);
+
+    // 게임 완료 시 결과 창으로 이동
+    useEffect(() => {
+        if (gameStatus === Status.SUCCESS) {
+            history.push('/result');
+        }
+    }, [gameStatus, history]);
+
+    return (
+        <>
+            {/* 게임 준비 페이지 */}
+            {gameStatus === Status.CLEAR && <Ready />}
+
+            {/* 게임 플레이 페이지 */}
+            {gameStatus === Status.LOADING && '게임 중'}
+        </>
+    );
 };
 
 export default Screen;
