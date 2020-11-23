8ee2579d043bd917c53db11bf12f5c5731207289
wtae1216 <wtae1216@gmail.com>
2020-11-23 14:02:38 +0900

    마지막 결과 노출

--- src/pages/result/index.tsx
+++ src/pages/result/index.tsx
@@ -1,11 +1,42 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useMemo } from 'react';
 import { shallowEqual, useSelector } from 'react-redux';
 
 import { useHistory } from 'libs/router';
+import styled from 'libs/styled';
 
 import { RootReducerState } from 'modules';
 
 import Status from 'constants/Status';
+
+import Button from 'components/Button';
+import SubTitle from 'components/SubTitle';
+
+const ResultStyle = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    height: 100%;
+`;
+
+const ScoreText = styled(SubTitle)`
+    font-size: 2.8rem;
+`;
+
+const AverageSecondText = styled.p`
+    color: ${({ theme }) => theme.colors.secondaryText};
+    font-size: 1.5rem;
+    font-weight: bold;
+
+    margin: 10px 0;
+`;
+
+const GameRestartButton = styled(Button)`
+    font-size: 2rem;
+    padding: 10px 30px;
+    margin-top: 20px;
+`;
 
 const Result: FC = () => {
     const history = useHistory();
@@ -30,7 +61,33 @@ const Result: FC = () => {
         }
     }, [history, status]);
 
-    return <div />;
+    // 평균 답변 시간 계산
+    const averageSecond = useMemo(() => {
+        const averageSecond =
+            seconds.length > 0
+                ? seconds.reduce((current, next) => current + next) /
+                  seconds.length
+                : 0;
+
+        return Math.floor(averageSecond * 10) / 10;
+    }, [seconds]);
+
+    return (
+        <ResultStyle>
+            {/* 최종 결과 */}
+            <ScoreText>
+                당신의 점수는 <span>{score}</span>점 입니다.
+            </ScoreText>
+
+            {/* 평균 답변 시간 */}
+            <AverageSecondText>
+                단어당 평균 답변 시간은 {averageSecond}초입니다.
+            </AverageSecondText>
+
+            {/* 다시 시작 버튼 */}
+            <GameRestartButton to="/">다시 시작</GameRestartButton>
+        </ResultStyle>
+    );
 };
 
 export default Result;
